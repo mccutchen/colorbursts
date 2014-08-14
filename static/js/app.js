34d21488fdc88c698807b419d83619431f0c5f0f
Will McCutchen <will@mccutch.org>
2014-08-14 11:33:17 -0400

    pull color gradient stuff into utils module

--- static/js/app.js
+++ static/js/app.js
@@ -1,37 +1,19 @@
 /** @jsx React.DOM */
 
 var React = require('react');
+
 var Fetcher = require('./fetcher');
+var Utils = require('./utils');
 
 var FETCH_INTERVAL = 30 * 1000;
 var UPDATE_INTERVAL = 1 * 1000;
 
 
-// background: linear-gradient(left, #fdd107 0, #fdd107 44%, #fdb614 44%, #fdb614 70%, #f47f1f 70%, #f47f1f 88%, #ee5725 88%, #ee5725 100%);
-// background: -webkit-linear-gradient(left, #fdd107 0, #fdd107 44%, #fdb614 44%, #fdb614 70%, #f47f1f 70%, #f47f1f 88%, #ee5725 88%, #ee5725 100%);
-// background: -moz-linear-gradient(left, #fdd107 0, #fdd107 44%, #fdb614 44%, #fdb614 70%, #f47f1f 70%, #f47f1f 88%, #ee5725 88%, #ee5725 100%);
-
-function colorsToGradient(colors) {
-    if (colors.length === 1) {
-        return colors.css;
-    }
-    var total = 0;
-    var stops = [];
-    for (var i = 0; i < colors.length; i++) {
-        var c = colors[i];
-        var pct = Math.ceil(c.weight * 100);
-        stops.push(c.css + ' ' + total + '%');
-        total += pct;
-        stops.push(c.css + ' ' + total + '%');
-    }
-    return '-webkit-linear-gradient(left, ' + stops.join(', ') + ')';
-}
-
 var Item = React.createClass({
     render: function() {
         var style = {
             'background-color': this.props.colors[0].css,
-            'background-image': colorsToGradient(this.props.colors)
+            'background-image': Utils.colorsToGradient(this.props.colors)
         };
         return (
             <div className="item" style={style}>
