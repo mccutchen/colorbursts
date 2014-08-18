1264fa1088be55cefc6bc957b7c285cdbecc7a34
Will McCutchen <will@mccutch.org>
2014-08-18 15:48:51 -0400

    fix gradient syntax per-browser

--- client/js/utils.js
+++ client/js/utils.js
@@ -1,6 +1,20 @@
-// background: linear-gradient(left, #fdd107 0, #fdd107 44%, #fdb614 44%, #fdb614 70%, #f47f1f 70%, #f47f1f 88%, #ee5725 88%, #ee5725 100%);
-// background: -webkit-linear-gradient(left, #fdd107 0, #fdd107 44%, #fdb614 44%, #fdb614 70%, #f47f1f 70%, #f47f1f 88%, #ee5725 88%, #ee5725 100%);
-// background: -moz-linear-gradient(left, #fdd107 0, #fdd107 44%, #fdb614 44%, #fdb614 70%, #f47f1f 70%, #f47f1f 88%, #ee5725 88%, #ee5725 100%);
+// Use something like so to get hard edges between gradients:
+// background: linear-gradient(left,
+//                             #fdd107 0,   #fdd107 44%,
+//                             #fdb614 44%, #fdb614 70%,
+//                             #f47f1f 70%, #f47f1f 88%,
+//                             #ee5725 88%, #ee5725 100%);
+
+var vendorPrefix = (function() {
+    var ua = navigator.userAgent;
+    if (/webkit/i.test(ua)) {
+        return '-webkit-';
+    } else if (/gecko/i.test(ua)) {
+        return '-moz-';
+    }
+    return '';
+})();
+
 
 function colorsToGradient(colors) {
     if (colors.length === 1) {
@@ -10,16 +24,15 @@ function colorsToGradient(colors) {
     var stops = [];
     for (var i = 0; i < colors.length; i++) {
         var c = colors[i];
-        var pct = Math.ceil(c.weight * 100);
+        var pct = Math.round(c.weight * 100);
         stops.push(c.css + ' ' + total + '%');
         total += pct;
         stops.push(c.css + ' ' + total + '%');
     }
-    var gradient = 'linear-gradient(left, ' + stops.join(', ') + ')';
-    var prefixes = ['', '-webkit-', '-moz-'];
-    return prefixes.map(function(prefix) {
-        return prefix + gradient;
-    }).join(', ');
+    // Because of an issue with React, we have to guess the single, vendor-
+    // prefixed version of the gradient rule to use:
+    // https://github.com/facebook/react/pull/723
+    return vendorPrefix + 'linear-gradient(left, ' + stops.join(', ') + ')';
 }
 
 module.exports = {
